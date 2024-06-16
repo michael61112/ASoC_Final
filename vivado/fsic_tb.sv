569f1cc3e11e217d8dc1c4b3d00b378a0242a46a
michael61112 <michael61112@yahoo.com.tw>
2024-06-16 12:16:56 -0400

    Pass simulation of vivado flow

--- vivado/fsic_tb.sv
+++ vivado/fsic_tb.sv
@@ -20,7 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `define SHOW_HEART_BEAT 1
- 
+//`define DEBUG 1 //
 import axi_vip_pkg::*;
 import design_1_axi_vip_0_0_pkg::*;
 import design_1_axi_vip_1_0_pkg::*;
@@ -91,6 +91,11 @@ module fsic_tb();
       end   
     `endif //SHOW_HEART_BEAT
 
+    initial begin
+        $dumpfile("log.vcd");
+        $dumpvars(0,fsic_tb);
+    end
+
     initial begin    
         fork
             system_reset_t();
@@ -122,20 +127,358 @@ module fsic_tb();
 
         @(is_txen_event);      
         $display($time, "=> Starting test...");
-/*
-//        Fpga2Soc_CfgRead();
-//        Fpga2Soc_CfgWrite();
-        FpgaLocal_CfgRead();
-        SocLocal_MbWrite();
-        FpgaLocal_MbWrite();
-        SocLa2DmaPath();
-        SocUp2DmaPath();
-*/
+
+        $display("TPU test from FPGA side");
+        TPU_test_pattern_FPGA(0);
+        TPU_test_pattern_FPGA(1);
+        TPU_test_pattern_FPGA(2);
+        TPU_test_pattern_FPGA(3);
+
+        $display("=============================================================================================");
+        $display("=============================================================================================");
+        $display("=============================================================================================");
+
         #500us    
         $display($time, "=> End of the test...");                         
         $finish;
     end
+
+//////////////////////////////////////////////////////////////////
+integer PATNUM;
+
+integer in_fd;
+integer patcount;
+integer nrow;
+integer i, j, k;
+integer err;
+integer temp;
+
+
+reg [127:0] GOLDEN [65535:0];
+
+reg [7:0] rbuf [3:0];
+reg [31:0] goldenbuf [3:0];
+reg [7:0] K_golden;
+reg [7:0] M_golden;
+reg [7:0] N_golden;
+
+reg [7:0]    M, K, N;
+
+parameter TPU_CTRL_OFFSET = 8'h00;
+
+parameter TPU_M_OFFSET = 8'h10;
+parameter TPU_K_OFFSET = 8'h14;
+parameter TPU_N_OFFSET = 8'h18;
+
+parameter TPU_BUFF_A_ADDR_OFFSET = 8'h20;
+parameter TPU_BUFF_A_DIN_OFFSET = 8'h24;
+parameter TPU_BUFF_B_ADDR_OFFSET = 8'h30;
+parameter TPU_BUFF_B_DIN_OFFSET = 8'h34;
+parameter TPU_BUFF_C_ADDR_OFFSET = 8'h40;
+parameter TPU_BUFF_C_DOUT_0_OFFSET = 8'h44;
+parameter TPU_BUFF_C_DOUT_1_OFFSET = 8'h48;
+parameter TPU_BUFF_C_DOUT_2_OFFSET = 8'h4c;
+parameter TPU_BUFF_C_DOUT_3_OFFSET = 8'h50;
+
+reg [31:0] read_data;
+reg [31:0] gbuff_A [65535:0];
+reg [31:0] gbuff_B [65535:0];
+reg [127:0] gbuff_C [65535:0];
+
+//assign read_data = data;
+
+task TPU_test_pattern_FPGA;
+  input [1:0] patternID;
+
+  $display("fsic_system_initial"); // Done by initial stage
+
+  $display("Enable TPU IP");
+  user_project_select(1);
+
+  $display("Program ap_start=0");
+  fpga_axilite_write_req(TPU_CTRL_OFFSET,  4'b1111, 32'd0); // in_valid = 1'b0;
+
+  $display("Wait TPU is idle");
+  tpu_waitIdle();
+
+  case (patternID)
+    2'b00: begin
+      in_fd = $fopen("../../../../../pattern/pattern0/input.txt", "r");
+    end
+    2'b01: begin
+      in_fd = $fopen("../../../../../pattern/pattern1/input.txt", "r");
+    end
+    2'b10: begin
+      in_fd = $fopen("../../../../../pattern/pattern2/input.txt", "r");
+    end
+    2'b11: begin
+      in_fd = $fopen("../../../../../pattern/pattern3/input.txt", "r");
+    end
+  endcase
+
+// PATNUM
+  temp = $fscanf(in_fd, "%d", PATNUM);
+
+  for(patcount = 0; patcount < PATNUM; patcount = patcount + 1) begin
+
+      // read input
+      read_and_write_cfg_to_userprj_fpga;
+      repeat(100) @ (posedge sys_clock);    //TODO fpga wait for write to soc
+
+      // start to feed data
+      repeat(3) @(negedge sys_clock);
+      $display("patcount[%d] ap_start = 1", patcount);
+      @(posedge sys_clock) fpga_axilite_write_req(TPU_CTRL_OFFSET, 4'b0001, 32'd1); // in_valid = 1'b1;
+
+      @(negedge sys_clock);
+      $display("patcount[%d] ap_start = 0", patcount);
+      @(posedge sys_clock) fpga_axilite_write_req(TPU_CTRL_OFFSET, 4'b0001, 32'd0); // in_valid = 1'b0;
+
+
+      wait_finished;
+
+      golden_check;
+
+      repeat(5) @(negedge sys_clock);
+  end
+
+  YOU_PASS_task;
+
+	endtask
+
+///////////////////////////////////////////////////////////////////////////////
+
+// Wait TPU is idle
+task tpu_waitIdle;
+  begin
+    fpga_axilite_read_req(TPU_CTRL_OFFSET, read_data);
+    while (read_data[2] != 1'b1) begin
+      @(posedge sys_clock);
+      fpga_axilite_read_req(TPU_CTRL_OFFSET, read_data);
+    end
+  end
+endtask
+
+task read_and_write_cfg_to_userprj_soc;
+
+  // read_KMN
+  temp = $fscanf(in_fd, "%h", K_golden);
+  temp = $fscanf(in_fd, "%h", M_golden);
+  temp = $fscanf(in_fd, "%h", N_golden);
+  $display("M: %d, K: %d N: %d", M_golden, K_golden, N_golden);
+
+  fpga_axilite_write_req(TPU_M_OFFSET, 4'b1111, M_golden);
+  fpga_axilite_write_req(TPU_K_OFFSET, 4'b1111, K_golden);
+  fpga_axilite_write_req(TPU_N_OFFSET, 4'b1111, N_golden);
+
+  // read_A_Matrix
+  nrow = (M_golden[1:0] !== 2'b00) ?  K_golden * ((M_golden>>2) + 1) : K_golden * (M_golden>>2);
+
+  for(i=0;i<nrow;i=i+1) begin
+    temp = $fscanf(in_fd, "%h %h %h %h", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
+    gbuff_A[i] = {rbuf[3], rbuf[2], rbuf[1], rbuf[0]};
+    $display("A[%d] = %h", i, gbuff_A[i]);
+
+    fpga_axilite_write_req(TPU_BUFF_A_ADDR_OFFSET, 4'b1111, i);
+    fpga_axilite_write_req(TPU_BUFF_A_DIN_OFFSET, 4'b1111, gbuff_A[i]);
+  end
+
+  // read_B_Matrix
+  nrow = (N_golden[1:0] !== 2'b00) ? K_golden * ((N_golden >> 2) + 1) : K_golden * (N_golden >> 2);
+
+  for(i=0;i<nrow;i=i+1) begin
+      temp = $fscanf(in_fd, "%h %h %h %h", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
+      gbuff_B[i] = {rbuf[3], rbuf[2], rbuf[1], rbuf[0]};
+      $display("B[%d] = %h", i, gbuff_B[i]);
+
+      fpga_axilite_write_req(TPU_BUFF_B_ADDR_OFFSET, 4'b1111, i);
+      fpga_axilite_write_req(TPU_BUFF_B_DIN_OFFSET, 4'b1111, gbuff_B[i]);
+  end
+
+  //read_golden
+  nrow = (N_golden[1:0] !== 2'b00) ? M_golden * ((N_golden>>2) + 1) : M_golden * (N_golden>>2);
+
+  for(i=0;i<nrow;i=i+1) begin
+      temp = $fscanf(in_fd, "%h %h %h %h", goldenbuf[3], goldenbuf[2], goldenbuf[1], goldenbuf[0]);
+      GOLDEN[i] = {goldenbuf[3], goldenbuf[2], goldenbuf[1], goldenbuf[0]};
+      $display("GOLDEN[%d] = %h %h %h %h", i, GOLDEN[i][127:96],  GOLDEN[i][95:64],  GOLDEN[i][63:32],  GOLDEN[i][31:0]);
+  end
+
+endtask
+
+task read_and_write_cfg_to_userprj_fpga;
+
+  // read_KMN
+  temp = $fscanf(in_fd, "%h", K_golden);
+  temp = $fscanf(in_fd, "%h", M_golden);
+  temp = $fscanf(in_fd, "%h", N_golden);
+  $display("M: %d, K: %d N: %d", M_golden, K_golden, N_golden);
+
+  fpga_axilite_write_req(TPU_M_OFFSET,  4'b1111, M_golden);
+  fpga_axilite_write_req(TPU_K_OFFSET,  4'b1111, K_golden);
+  fpga_axilite_write_req(TPU_N_OFFSET,  4'b1111, N_golden);
+
+  // read_A_Matrix
+  nrow = (M_golden[1:0] !== 2'b00) ?  K_golden * ((M_golden>>2) + 1) : K_golden * (M_golden>>2);
+
+  for(i=0;i<nrow;i=i+1) begin
+      temp = $fscanf(in_fd, "%h %h %h %h", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
+      gbuff_A[i] = {rbuf[3], rbuf[2], rbuf[1], rbuf[0]};
+      $display("A[%d] = %h", i, gbuff_A[i]);
+
+      fpga_axilite_write_req(TPU_BUFF_A_ADDR_OFFSET,  4'b1111, i);
+      fpga_axilite_write_req(TPU_BUFF_A_DIN_OFFSET,  4'b1111, gbuff_A[i]);
+  end
+
+  // read_B_Matrix
+  nrow = (N_golden[1:0] !== 2'b00) ? K_golden * ((N_golden >> 2) + 1) : K_golden * (N_golden >> 2);
+
+  for(i=0;i<nrow;i=i+1) begin
+      temp = $fscanf(in_fd, "%h %h %h %h", rbuf[3], rbuf[2], rbuf[1], rbuf[0]);
+      gbuff_B[i] = {rbuf[3], rbuf[2], rbuf[1], rbuf[0]};
+      $display("B[%d] = %h", i, gbuff_B[i]);
+
+      fpga_axilite_write_req(TPU_BUFF_B_ADDR_OFFSET,  4'b1111, i);
+      fpga_axilite_write_req(TPU_BUFF_B_DIN_OFFSET,  4'b1111, gbuff_B[i]);
+  end
+
+  //read_golden
+  nrow = (N_golden[1:0] !== 2'b00) ? M_golden * ((N_golden>>2) + 1) : M_golden * (N_golden>>2);
+
+  for(i=0;i<nrow;i=i+1) begin
+      temp = $fscanf(in_fd, "%h %h %h %h", goldenbuf[3], goldenbuf[2], goldenbuf[1], goldenbuf[0]);
+      GOLDEN[i] = {goldenbuf[3], goldenbuf[2], goldenbuf[1], goldenbuf[0]};
+      $display("GOLDEN[%d] = %h %h %h %h", i, GOLDEN[i][127:96],  GOLDEN[i][95:64],  GOLDEN[i][63:32],  GOLDEN[i][31:0]);
+  end
+
+endtask
+
+task wait_finished; begin
+
+    fpga_axilite_read_req(TPU_CTRL_OFFSET, read_data);
+    while (read_data[1] != 1'b1) begin // ap_done == 1'b1
+
+        fpga_axilite_read_req(TPU_CTRL_OFFSET, read_data);
+    end
+
+end endtask
+
+
+task golden_check; begin
+
+    err = 0;
+
+    // Read C Buffer
+    for(i=0;i<nrow;i=i+1) begin
+      fpga_axilite_write_req(TPU_BUFF_C_ADDR_OFFSET, 4'b1111, i);
+      repeat (100) @(posedge sys_clock);
+      fpga_axilite_read_req(TPU_BUFF_C_DOUT_0_OFFSET, read_data);
+			gbuff_C[i][31:0] = read_data;
+      fpga_axilite_read_req(TPU_BUFF_C_DOUT_1_OFFSET, read_data);
+			gbuff_C[i][63:32] = read_data;
+      fpga_axilite_read_req(TPU_BUFF_C_DOUT_2_OFFSET, read_data);
+			gbuff_C[i][95:64] = read_data;
+      fpga_axilite_read_req(TPU_BUFF_C_DOUT_3_OFFSET, read_data);
+			gbuff_C[i][127:96] = read_data;
+
+      $display("C[%d] = %h", i, gbuff_C[i]);
+    end
+
+    nrow = (N_golden[1:0] !== 2'b00) ? M_golden * ((N_golden>>2) + 1) : M_golden * (N_golden>>2);
+    for(i = 0; i < nrow; i=i+1) begin
+        if(GOLDEN[i][127:96] !== gbuff_C[i][127:96]) begin
+            $display("gbuff[%d][127:96] = %8h, expect = %8h", i, gbuff_C[i][127:96], GOLDEN[i][127:96]);
+            err = err + 1;
+        end
+
+        if(GOLDEN[i][95:64] !== gbuff_C[i][95:64]) begin
+            $display("gbuff[%d][95:64] = %8h, expect = %8h", i, gbuff_C[i][95:64], GOLDEN[i][95:64]);
+            err = err + 1;
+        end
+
+        if(GOLDEN[i][63:32] !== gbuff_C[i][63:32]) begin
+            $display("gbuff[%d][63:32] = %8h, expect = %8h", i, gbuff_C[i][63:32], GOLDEN[i][63:32]);
+            err = err + 1;
+        end
+
+        if(GOLDEN[i][31:0] !== gbuff_C[i][31:0]) begin
+            $display("gbuff[%d][31:0] = %8h, expect = %8h", i, gbuff_C[i][31:0], GOLDEN[i][31:0]);
+            err = err + 1;
+        end
+    end
+
+    if(err != 0) begin
+        wrong_ans;
+    end
+end endtask
+
+
+task wrong_ans; begin
+    $display("-------------------------------------This is wrong answer-------------------------------------");
+    $finish;
+end endtask
+
+task YOU_PASS_task; begin
+    $display("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Yes, you pass answoer   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+end endtask
+// ***************************************************************
+
+task user_project_select;
+    input [4:0] up_index;
+    begin
+        $display($time, "=> Fpga2Soc_Write: SOC_CC");
+        offset = 0;
+        data = up_index;
+        axil_cycles_gen(WriteCyc, SOC_CC, offset, data, 1);
+
+        `ifdef DEBUG
+            axil_cycles_gen(ReadCyc, SOC_CC, offset, data, 1);
+            if(data == up_index) begin
+                $display($time, "=> Fpga2Soc_Write SOC_CC offset %h = %h, PASS", offset, data);
+            end else begin
+                $display($time, "=> Fpga2Soc_Write SOC_CC offset %h = %h, FAIL", offset, data);
+                ->> error_event;
+            end
+            $display($time, "=> user_project %d is enable", up_index);
+        `endif
+    end
+endtask
+
+task fpga_axilite_write_req;
+    input [27:0] address;
+    input [3:0] BE;
+    input [31:0] in_data;
+
+    offset = {BE,address};
+
+    $display($time, "=> WriteCyc, offset %h = %h, PASS", offset, in_data);
+    axil_cycles_gen(WriteCyc, SOC_UP, offset, in_data, 1);
+
+    `ifdef DEBUG
+        $display($time, "=> ReadCyc");
+        axil_cycles_gen(ReadCyc, SOC_UP, offset, data, 1);
+        $display($time, "=> ReadCyc done");
+        if(data == in_data) begin
+            $display($time, "=> Fpga2Soc_Write offset %h = %h, PASS", offset, data);
+        end else begin
+            $display($time, "=> Fpga2Soc_Write offset %h = %h, FAIL", offset, data);
+           // ->> error_event;
+        end
+    `endif
+endtask
+
+task fpga_axilite_read_req;
+    input [11:0] offset;
+    output [31:0] data;
+
+    axil_cycles_gen(ReadCyc, SOC_UP, offset, data, 1);
     
+    `ifdef DEBUG
+            $display($time, "=> Fpga2Soc_Read SOC_UP offset %h = %h, PASS", offset, data);
+    `endif
+endtask
+
+//////////////////////////////////////////////////////////////////
     task system_reset_t;
         begin
             sys_reset = 0;
